01c94ad71fe9f05947f950d1d7d0044d058141a9
Egor Gorbik <gorbikega@gmail.com>
2018-09-30 14:44:44 +0300

    add additional_5 task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+arr = str.split('');
+for (var i = 0; i < str.length; i++){
+    for (var j = 0; j < bracketsConfig.length; j++)
+        if (arr[i] == bracketsConfig[j][0] && arr[i+1] == bracketsConfig[j][1]){
+            arr.splice(i,2);
+            i = -1;
+    }
+}
+
+if (arr.length == 0){
+  return true;
+}
+else return false;
+
 }
